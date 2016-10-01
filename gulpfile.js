e0dcafb996589a700bf2bba756e289656399f209
Faz Bnei <b2027642@my.shu.ac.uk>
2016-10-01 23:53:23 +0100

    Combined js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
-	coffee = require('gulp-coffee');
+	coffee = require('gulp-coffee'),
+	concat = require('gulp-concat');
 
 
 var coffeeSources = ['_components/coffee/tagline.coffee'];
+var jsSources = ['_components/scripts/rclick.js',
+'_components/scripts/pixgrid.js',
+'_components/scripts/tagline.js',
+'_components/scripts/template.js'];
 
 gulp.task('coffee', function(){
 	gulp.src(coffeeSources)
@@ -11,4 +16,13 @@ gulp.task('coffee', function(){
 	.on('error', gutil.log))
 	.pipe(gulp.dest('_components/scripts'))
 	
+});
+
+
+
+
+gulp.task('js',function () {
+	gulp.src(coffeeSources)
+	.pipe(concat('script.js'))
+	.pipe(gulp.dest('_builds/_components/js'))
 });
